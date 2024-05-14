2944f8333bebd317bc666c5a6c7d641484d1c88c
Arjun Suthar <arjunsuthariit@gmail.com>
2024-05-14 20:05:57 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,70 +1,60 @@
-# Getting Started with Create React App
+## React Calculator App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a simple calculator application built using React. It allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division.
 
-## Available Scripts
+**Key Features:**
 
-In the project directory, you can run:
+- Addition (+), subtraction (-), multiplication (X), and division (/) operations
+- Clear all (AC) and backspace (←) functionality
+- Percentage (%) and decimal (.) support
+- EChange sign (+/-) functionality
 
-### `npm start`
+**Table of Contents**
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- [Getting Started](#getting-started)
+- [Running the Application](#running-the-application)
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+**Built with:**
 
-### `npm test`
+- React
+- React Hooks (useState)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Getting Started
 
-### `npm run build`
+Follow these instructions to get a copy of the project up and running on your local machine for development and testing purposes.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### Prerequisites
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- Node.js (version 14 or later) and npm (or yarn) installed on your system. You can download them from [https://nodejs.org/en/download](https://nodejs.org/en/download)
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+### Installation
 
-### `npm run eject`
+1. Clone the repository:
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+```bash
+git clone []()
+```
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+2. Navigate to the project directory:
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+```bash
+cd calculator
+```
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+3. Install dependencies:
 
-## Learn More
+```bash
+npm install
+```
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+## Running the Application
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+1. Start the development server:
 
-### Code Splitting
+```bash
+npm start
+```
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+This will launch the development server and open BusyBuy in your default browser, usually at http://localhost:3000 (the port might vary).
 
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+I hope this readme file is helpful🙃
--- src/App.js
+++ src/App.js
@@ -1,23 +1,192 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import { BsBackspaceFill } from "react-icons/bs";
 function App() {
+  const [result, setResult] = useState("0");
+  const [firstValue, setFirstValue] = useState("0");
+  const [secondValue, setSecondValue] = useState("");
+  const [operator, setOperator] = useState("");
+  const [lastExpression, setLastExpression] = useState("0");
+
+  const handleNumberClick = (num) => {
+    if (operator === "") {
+      setFirstValue((preValue) => preValue + num);
+      setResult((preResult) => (preResult === "0" ? num : preResult + num));
+    } else {
+      setSecondValue((preValue) => preValue + num);
+      setResult(secondValue + num);
+    }
+    setLastExpression((preExpression) => preExpression + num);
+  };
+
+  const handleOperatorClick = (op) => {
+    if (operator !== "" && secondValue !== "") {
+      calculateResult();
+    }
+
+    setOperator(op);
+
+    if (!lastExpression.trim().endsWith(op)) {
+      setLastExpression(
+        (prevExpression) => prevExpression.replace(/\s+$/, "") + ` ${op} `
+      );
+    }
+  };
+
+  const calculateResult = () => {
+    const num1 = parseFloat(firstValue);
+    const num2 = parseFloat(secondValue);
+
+    let calculatedResult = 0;
+    switch (operator) {
+      case "+":
+        calculatedResult = num1 + num2;
+        break;
+      case "-":
+        calculatedResult = num1 - num2;
+        break;
+      case "X":
+        calculatedResult = num1 * num2;
+        break;
+      case "/":
+        calculatedResult = num1 / num2;
+        break;
+      case "%":
+        calculatedResult = (parseFloat(num1) / 100) * num2.toString();
+        break;
+      default:
+        break;
+    }
+
+    setResult(calculatedResult.toString());
+    setFirstValue(calculatedResult.toString());
+    setLastExpression(calculatedResult.toString());
+    setSecondValue("");
+  };
+
+  const handleClear = () => {
+    setResult("0");
+    setFirstValue("0");
+    setSecondValue("");
+    setOperator("");
+    setLastExpression("0");
+  };
+
+  const handleBackspace = () => {
+    if (operator === "") {
+      setFirstValue((prevValue) => prevValue.slice(0, -1));
+      setResult((prevResult) =>
+        prevResult.length === 1 ? "0" : prevResult.slice(0, -1)
+      );
+      setLastExpression((prevExpression) =>
+        prevExpression.slice(0, -1).replace(/\s+$/, "")
+      );
+    } else {
+      setSecondValue((prevValue) => prevValue.slice(0, -1));
+      setResult((prevResult) =>
+        prevResult.length === 1 ? "0" : prevResult.slice(0, -1)
+      );
+      setLastExpression((prevExpression) =>
+        prevExpression.slice(0, -1).replace(/\s+$/, "")
+      );
+    }
+  };
+
+  const handleDecimal = () => {
+    if (operator === "") {
+      if (!firstValue.includes(".")) {
+        setFirstValue((prevValue) => prevValue + ".");
+        setResult((prevResult) => prevResult + ".");
+        setLastExpression((prevExpression) => prevExpression + ".");
+      }
+    } else {
+      if (!secondValue.includes(".")) {
+        setSecondValue((prevValue) => prevValue + ".");
+        setResult((prevResult) => prevResult + ".");
+        setLastExpression((prevExpression) => prevExpression + ".");
+      }
+    }
+  };
+
+  const handleToggleSign = () => {
+    if (operator === "") {
+      setFirstValue((parseFloat(result) * -1).toString());
+    } else {
+      setSecondValue((parseFloat(result) * -1).toString());
+    }
+    setResult((parseFloat(result) * -1).toString());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className={(`${"screen"}`, `${"lastExpression"}`)}>
+          {lastExpression}
+        </div>
+        <div className="screen">{result}</div>
+        <div className="keysContainer">
+          <div className="key" onClick={handleClear}>
+            AC
+          </div>
+          <div className="key" onClick={handleBackspace}>
+            <BsBackspaceFill />
+          </div>
+          <div className="key" onClick={handleToggleSign}>
+            +/_
+          </div>
+          <div className="key" onClick={() => handleOperatorClick("/")}>
+            /
+          </div>
+          <div className="key" onClick={() => handleNumberClick("7")}>
+            7
+          </div>
+          <div className="key" onClick={() => handleNumberClick("8")}>
+            8
+          </div>
+          <div className="key" onClick={() => handleNumberClick("9")}>
+            9
+          </div>
+          <div className="key" onClick={() => handleOperatorClick("X")}>
+            X
+          </div>
+          <div className="key" onClick={() => handleNumberClick("4")}>
+            4
+          </div>
+          <div className="key" onClick={() => handleNumberClick("5")}>
+            5
+          </div>
+          <div className="key" onClick={() => handleNumberClick("6")}>
+            6
+          </div>
+          <div className="key" onClick={() => handleOperatorClick("-")}>
+            -
+          </div>
+          <div className="key" onClick={() => handleNumberClick("1")}>
+            1
+          </div>
+          <div className="key" onClick={() => handleNumberClick("2")}>
+            2
+          </div>
+          <div className="key" onClick={() => handleNumberClick("3")}>
+            3
+          </div>
+          <div className="key" onClick={() => handleOperatorClick("+")}>
+            +
+          </div>
+          <div className="key" onClick={() => handleOperatorClick("%")}>
+            %
+          </div>
+          <div className="key" onClick={() => handleNumberClick("0")}>
+            0
+          </div>
+          <div className="key" onClick={handleDecimal}>
+            .
+          </div>
+          <div className="key" onClick={calculateResult}>
+            =
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
